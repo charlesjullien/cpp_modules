720098bf775524728d6e665ccab9e1092bd8eb36
charlesjullien <61630987+charlesjullien@users.noreply.github.com>
2022-04-19 10:47:13 +0200

    Add files via upload

---
+++ cpp07/ex00/includes/whatever.hpp
@@ -0,0 +1,32 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+#include <iostream>
+#include <string>
+//les templates se mettent toujours dans le .hpp
+template<typename T>//déclaration générique d'un template
+void swap(T &t1, T &t2)//peu importe le type de t1 ou t2... char, int, double, char*... ca va fonctionner
+{
+	T temp;
+	temp = t1;
+	t1 = t2;
+	t2 = temp;
+}
+
+template<typename T>
+T min(T t1, T t2)
+{
+	if (t1 < t2)
+		return t1;
+	return t2;
+}
+
+template<class T>
+T max(T t1, T t2)
+{
+	if (t1 > t2)
+		return t1;
+	return t2;
+}
+
+#endif
---
+++ cpp07/ex00/srcs/main.cpp
@@ -0,0 +1,22 @@
+#include "../includes/whatever.hpp"
+
+int main(int ac, char **av)
+{
+	double a = -100.42;
+	double b = 1.618;
+	swap(a, b);
+	std::cout << "a == " << a << ", b == " << b << std::endl;
+	std::cout << "min a or b == " << ::min(a, b) << std::endl;
+	std::cout << "max a or b == " << ::max(a, b) << std::endl;
+
+	std::cout << std::endl;
+
+	std::string c = "Bonjour";
+	std::string d = "Aurevoir";
+	swap(c, d);
+	std::cout << "c == |" << c << "| && d == |" << d << "|" << std::endl;
+	std::cout << "min c or d == " << ::min(c, d) << std::endl;
+	std::cout << "max c or d == " << ::max(c, d) << std::endl;
+
+	return (0);	
+}
